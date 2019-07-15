6921952382d4bc2184c69cb8ea7b8f40f8cb9ed8
Everton Paiva <evertonpaiva@gmail.com>
2019-07-15 11:38:27 -0300

    Incluindo um plugin

--- produtos/src/main/java/br/com/alura/maven/Produto.java
+++ produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -4,6 +4,7 @@ public class Produto {
 
 	private final String nome;
 	private final double preco;
+	private final String categoria = "comida";
 	
 	public Produto(String nome, double preco) {
 		super();
@@ -11,5 +12,11 @@ public class Produto {
 		this.preco = preco;
 	}
 	
+	public String getNome() {
+		return nome;
+	}
 	
+	public double getPreco() {
+		return preco;
+	}
 }
